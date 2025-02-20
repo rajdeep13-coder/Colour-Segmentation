426fdbe7088b939e0238b68b9338b70ef98f5b85
Gaurav Karakoti <karakotigaurav12@gmail.com>
2025-02-20 21:51:10 +0530

    Final

--- segment.py
+++ segment.py
@@ -1,18 +1,36 @@
 import cv2
 import numpy as np
-img=np.zeros((300,512,3),np.uint8)
-# frame=cv2.imread("image1.webp",-1)
 def nothing(x):
-    return
-cv2.namedWindow("Colour Segmentation")
-cv2.createTrackbar("R","Colour Segmentation",0,255,nothing)
-cv2.createTrackbar("G","Colour Segmentation",0,255,nothing)
-cv2.createTrackbar("B","Colour Segmentation",0,255,nothing)
-while(True):
-    cv2.imshow('Colour Segmentation',img)
-    r=cv2.getTrackbarPos("R","Colour Segmentation")
-    g=cv2.getTrackbarPos("G","Colour Segmentation")
-    b=cv2.getTrackbarPos("B","Colour Segmentation")
-    img[:]=[b,g,r]
-    cv2.waitKey(1)
+    pass
+cap = cv2.VideoCapture("video1.mp4")
+cv2.namedWindow("Tracking")
+cv2.createTrackbar("LH", "Tracking", 0, 179, nothing)
+cv2.createTrackbar("LS", "Tracking", 0, 255, nothing)
+cv2.createTrackbar("LV", "Tracking", 0, 255, nothing)
+cv2.createTrackbar("UH", "Tracking", 179, 179, nothing)
+cv2.createTrackbar("US", "Tracking", 255, 255, nothing)
+cv2.createTrackbar("UV", "Tracking", 255, 255, nothing)
+while True:
+    ret, img = cap.read()
+    if not ret:
+        print("Video has ended.")
+        break
+    img = cv2.resize(img, (512, 512))
+    l_h = cv2.getTrackbarPos("LH", "Tracking")
+    l_s = cv2.getTrackbarPos("LS", "Tracking")
+    l_v = cv2.getTrackbarPos("LV", "Tracking")
+    u_h = cv2.getTrackbarPos("UH", "Tracking")
+    u_s = cv2.getTrackbarPos("US", "Tracking")
+    u_v = cv2.getTrackbarPos("UV", "Tracking")
+    l_b = np.array([l_h, l_s, l_v])
+    u_b = np.array([u_h, u_s, u_v])
+    mask = cv2.inRange(img,l_b,u_b)
+    res = cv2.bitwise_and(img, img, mask=mask)
+    cv2.imshow("frame", img)
+    cv2.imshow("mask", mask)
+    cv2.imshow("res", res)
+    key=cv2.waitKey(1)
+    if key==27:
+        break
+cap.release()
 cv2.destroyAllWindows()
